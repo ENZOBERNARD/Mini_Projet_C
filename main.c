f22c9aba5c4ab1e76a11fda250bb14dff757127d
Enzo <bernardenzo99@gmail.com>
2020-10-28 12:33:30 +0100

    +Jump

--- main.c
+++ main.c
@@ -1,14 +1,36 @@
 #include "raylib.h"
 
+   typedef struct Player {
+    Vector2 position;
+    float speed;
+    bool canJump;
+    int jumpState;
+    int jumpFrame;
+    } Player;
+    
+    typedef struct Obstacle {
+    Vector2 position;
+    float speed;
+    } Obstacle;
+   
+
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
+
+
     const int screenWidth = 800;
     const int screenHeight = 450;
     
-    Vector2 ballPosition = { (float)100, (float)320};
-    Vector2 ObstaclePosition= {(float)700, (float(320) };
+    Player player;
+    Obstacle obstacle;
+    player.position = (Vector2){(float)100, (float)320};
+    obstacle.position= (Vector2){(float)700, (float)270};
+    obstacle.speed=1;
+    bool pause=true;
+    player.jumpState=0;
+    player.jumpFrame=0;
     
     
     
@@ -23,23 +45,19 @@ int main(void)
     {
         // Update
         //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
-        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
-        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
-        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
+        if (IsKeyPressed(KEY_ENTER)) pause = !pause;
+        if (pause==false)gameRun(&obstacle);
+        jump(&player);
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
-
             ClearBackground(RAYWHITE);
-
             DrawText("Run!", 350,40,50, RED);
-            DrawCircle(ballPosition.x, ballPosition.y, 30, DARKBLUE);
+            DrawCircle(player.position.x, player.position.y, 30, DARKBLUE);
             DrawLine(0,350, screenWidth,350, BLACK);
-            DrawRectangle(ObstaclePosition.x,ObstaclePosition.y,50,80,BLACK);
-
+            DrawRectangle(obstacle.position.x,obstacle.position.y,50,80,BLACK);
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
@@ -50,4 +68,40 @@ int main(void)
     //--------------------------------------------------------------------------------------
 
     return 0;
+}
+
+
+
+void gameRun(Obstacle * obstacle){
+    obstacle->position.x =obstacle->position.x-2*obstacle->speed;
+    
+}
+
+void jump(Player * player){
+    switch (player->jumpState){
+        case 0 :
+            if(IsKeyDown(KEY_SPACE)){
+                player->jumpState=1;
+            }
+            break;
+        case 1 :
+            if(player->jumpFrame<=20){
+                player->position.y -=5;
+                player->jumpFrame++;
+            }
+            else{
+                player->jumpState=2;
+            }
+            break;
+        case 2:
+            if(player->position.y<320){
+                player->position.y +=5;
+                player->jumpFrame++;
+            }
+            else{
+                player->jumpState=0;
+                player->jumpFrame=0;
+            }
+            break;
+    }
 }
